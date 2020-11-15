185f139eb829dbb7ac894e11e7d6952a7e29070d
Yordanov <iliyanyyordanov@gmail.com>
2020-11-15 14:06:01 +0000

    Title: Refactoring to App.js and index.js
    Problem
    App.js was only a boilerplate
    
    index.js required import some of the installed libraries
    
    Solution
    App.js - Place the components in place and Switch from react-router-dom
    
    index.js - imported the required libraries and the css file
    
    Signed-off-by: Yordanov <iliyanyyordanov@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+//Import react and any used hooks
+import React, { useState } from 'react';
 
-function App() {
+//Import React-router-dom 
+import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
+
+//Import spring animation
+import {useSpring, animated} from 'react-spring'
+
+//Import Components
+import NavBar from './components/navBar';
+import SideNav from './components/sideNav';
+import Footer from './components/footer';
+
+//Components for routing
+import Dashboard from './components/dashboard';
+import AddUser from './components/addUser';
+import AllUsers from './components/allUsers';
+import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
+import ForgotPassword from './components/forgotPassword';
+import PageNotFound from './components/pageNotFound';
+import InternalServerError from './components/internalServerError';
+import AccessDenied from './components/accessDenied';
+import Charts from './components/charts';
+import Tables from './components/tables';
+
+const App = () => {
+  //Define states:
+  const [open,setOpen] = useState(true)
+
+  //Define the animation of the sidenav
+  const animateSideNav = useSpring({left: open ? 0 : -500, display: open ? "block" : "none" ,from: {left: open ? 0 : -500}})
+
+  //Render the components
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+            {/* Navigation bar */}
+            <NavBar setOpen={setOpen} />
+
+            <div id="layoutSidenav">
+
+                {/* Sidebar navigation */}
+                <animated.div style={animateSideNav} id="layoutSidenav_nav">
+                    <SideNav />
+                </animated.div>
+                <div id="layoutSidenav_content">
+                  
+              {/* Main section */}
+                <main>
+                {/* React-router-dom switch*/}
+                  <Switch>
+                    <Route exact path="/tables">
+                      <Tables />
+                    </Route>
+                    <Route exact path="/charts">
+                      <Charts />
+                    </Route>
+                    <Route exact path="/internal-server-error">
+                      <InternalServerError />
+                    </Route>
+                    <Route exact path="/access-denied">
+                      <AccessDenied />
+                    </Route>
+                    <Route exact path="/page-not-found">
+                      <PageNotFound />
+                    </Route>
+                    <Route  path="/forgot-password">
+                      <ForgotPassword />
+                    </Route>
+                    <Route  path="/register">
+                      <RegisterForm />
+                    </Route>
+                    <Route  path="/login">
+                      <LoginForm />
+                    </Route>
+                    <Route  path="/all-users">
+                      <AllUsers />
+                    </Route>
+                    <Route  path="/add-user">
+                      <AddUser />
+                    </Route>
+                    <Route exact path="/">
+                      <Dashboard />
+                    </Route>
+                    <Route exact path="*">
+                      <PageNotFound />
+                    </Route>
+                  </Switch>     
+                </main>
+
+                    {/* Footer */}
+                    <Footer />
+                    
+                </div>
+            </div>
+        </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./css/index.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap";
+import "popper.js/dist/popper";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
